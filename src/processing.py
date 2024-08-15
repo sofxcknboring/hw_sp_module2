7950642fc609cda8565154285e6cd55e25001704
v.mishurenko <paranoiawe@gmail.com>
2024-08-15 07:29:22 +0300

    refactor: change variable name in sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -9,12 +9,12 @@ def filter_by_state(bank_operations: list[dict], state: str = "EXECUTED") -> lis
     return [i for i in bank_operations if i["state"] == state]
 
 
-def sort_by_date(bank_transactions: list[dict], sort_order: bool = True) -> list[dict]:
+def sort_by_date(bank_operations: list[dict], sort_order: bool = True) -> list[dict]:
     """
     Сортирует банковские операции по дате.
 
-    :param bank_transactions: Список операций (словарей).
+    :param bank_operations: Список операций (словарей).
     :param sort_order: Условие для сортировки (по умолчанию сортирует по убыванию - True).
     :return: Новый отсортированный список операций
     """
-    return sorted(bank_transactions, key=lambda x: x["date"], reverse=sort_order)
+    return sorted(bank_operations, key=lambda x: x["date"], reverse=sort_order)
