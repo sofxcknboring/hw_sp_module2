2aa7a52fcbbc9a55fd9c6b627ea58ffcd7011858
v.mishurenko <paranoiawe@gmail.com>
2024-08-21 20:22:06 +0300

    add: module src/generators.py, tests/test_generators.py

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,19 +1,13 @@
 import pytest
 
+from src.generators import filter_by_currency
+from tests.conftest import transactions
 
-@pytest.fixture
-def bank_operations():
-    return [
-        {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-        {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-        {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-        {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-    ]
 
-@pytest.fixture
-def transactions():
-    return [
-        {
+def test_filter_by_currency_valid(transactions):
+    iter_usd_value = filter_by_currency(transactions, 'USD')
+    iter_rub_value = filter_by_currency(transactions, 'RUB')
+    assert next(iter_usd_value) == {
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
@@ -27,8 +21,8 @@ def transactions():
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
             "to": "Счет 11776614605963066702"
-        },
-        {
+        }
+    assert next(iter_usd_value) == {
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
@@ -42,23 +36,8 @@ def transactions():
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
             "to": "Счет 75651667383060284188"
-        },
-        {
-            "id": 873106923,
-            "state": "EXECUTED",
-            "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
-            "description": "Перевод со счета на счет",
-            "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
-        },
-        {
+        }
+    assert next(iter_usd_value) == {
             "id": 895315941,
             "state": "EXECUTED",
             "date": "2018-08-19T04:27:37.904916",
@@ -72,8 +51,23 @@ def transactions():
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
             "to": "Visa Platinum 8990922113665229"
-        },
-        {
+        }
+    assert next(iter_rub_value) == {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }
+    assert next(iter_rub_value) == {
             "id": 594226727,
             "state": "CANCELED",
             "date": "2018-09-12T21:27:25.241689",
@@ -88,4 +82,16 @@ def transactions():
             "from": "Visa Platinum 1246377376343588",
             "to": "Счет 14211924144426031657"
         }
-    ]
+    with pytest.raises(StopIteration):
+        next(iter_rub_value)
+
+def test_filter_by_currency_no_currency_name(transactions):
+    iter_eur_value = filter_by_currency(transactions, 'EUR')
+    with pytest.raises(StopIteration):
+        next(iter_eur_value)
+
+def test_filter_by_currency_no_transactions():
+    empty_transactions = []
+    empty_result = list(filter_by_currency(empty_transactions, 'USD'))
+
+    assert empty_result == empty_transactions
