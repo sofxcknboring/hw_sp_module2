08a1ca397aabed05e5133651c817f8ca37e278f7
v.mishurenko <paranoiawe@gmail.com>
2024-09-03 23:32:16 +0300

    update load_transactions_from_json

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,10 @@ import json
 
 
 def load_transactions_from_json(file_path: str) -> list[dict]:
+    """
+    :param file_path: путь до JSON-файла
+    :return: Список словарей с данными о финансовых транзакциях
+    """
     with open(file_path, 'r') as file:
         transactions_data = json.load(file)
     return transactions_data if type(transactions_data) == list else []
