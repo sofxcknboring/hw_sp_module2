b79a5cd16bc2337cb56fabf92d5d2434db985a8b
v.mishurenko <paranoiawe@gmail.com>
2024-09-04 01:05:05 +0300

    create external_api, add convert_amount_to_rub

---
+++ src/external_api.py
@@ -0,0 +1,25 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def convert_amount_to_rub(transaction: dict) -> float:
+    """
+    Конвертирует валюту в рубли.
+    :param transaction: Словарь с данными транзакции.
+    :return: Сумма(transaction["amount"]) в рублях.
+    """
+    currency = transaction.get("operationAmount").get("currency").get("code")
+    amount = transaction.get("operationAmount").get("amount")
+    headers = {"apikey": os.getenv("API_KEY")}
+
+    if currency != "RUB":
+        url = f"https://api.apilayer.com/exchangerates_data/convert?to=RUB&from={currency}&amount={amount}"
+        response = requests.get(url, headers=headers)
+        response_data = response.json()
+        return response_data["result"]
+
+    return amount
--- src/utils.py
+++ src/utils.py
@@ -6,7 +6,6 @@ def load_transactions_from_json(file_path: str) -> list[dict]:
     :param file_path: путь до JSON-файла
     :return: Список словарей с данными о финансовых транзакциях
     """
-    with open(file_path, 'r') as file:
+    with open(file_path, "r") as file:
         transactions_data = json.load(file)
     return transactions_data if type(transactions_data) == list else []
-
