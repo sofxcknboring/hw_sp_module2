431aff2e0c6441d9b5cc479b813b6ac3ce0c4b45
v.mishurenko <paranoiawe@gmail.com>
2024-09-15 16:24:48 +0300

    update decorator.py log

--- src/decorators.py
+++ src/decorators.py
@@ -9,20 +9,17 @@ def log(filename: Optional[str] = None) -> Callable:
     """
 
     def wrapper(func: Callable) -> Callable:
-        def inner(*args: Any, **kwargs: Any) -> None:
-            message = ""
+        def inner(*args: Any, **kwargs: Any) -> Any:
             try:
                 func(*args, **kwargs)
                 message = f"{func.__name__} ok"
             except Exception as e:
                 message = f"{func.__name__} error: {e}. Inputs: {args}, {kwargs}"
-
             if filename:
                 with open(filename, "a") as file:
                     file.write(message + "\n")
             else:
                 print(message)
-
+            return func(*args, **kwargs)
         return inner
-
     return wrapper
